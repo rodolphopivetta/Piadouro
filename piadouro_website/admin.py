258b9527f3ebb50a79972ba60497df4c19306625
Alvaro <alvaro.lqueiroz@gmail.com>
2014-11-06 15:16:18 -0200

    Up

--- piadouro_website/admin.py
+++ piadouro_website/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from piadouro_website.models import Piado
+from piadouro_website.models import Piado, Follow
 
 # Register your models here.
 admin.site.register(Piado)
+admin.site.register(Follow)
